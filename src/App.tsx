d24dd36e00964e27629b3e7bd3dbae09a49fb036
강은빈 <kub0803@naver.com>
2022-01-16 04:40:05 +0900

    [ADD] toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import "./App.css";
 import { FeedPage, MapPage, SignPage, SignUpPage } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
+    <ToastContainer />
       <Routes>
         <Route path="/" element={<MapPage />} />
         <Route path="/signin" element={<SignPage />} />
