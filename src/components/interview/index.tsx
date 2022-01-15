857fe632cd4f0ceeac814ccab451410173c3a98b
yelim <choyr2016@naver.com>
2022-01-15 19:04:18 +0900

    [ADD] interview componentes add

---
+++ src/components/interview/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const PostInterviewPage = () => {
+  return (
+    <S.Wrapper>
+      <button></button>
+    </S.Wrapper>
+  );
+};
+
+export default PostInterviewPage;
---
+++ src/components/interview/style.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  button {
+    width: 50px;
+    height: 30px;
+    background-color: blue;
+  }
+`;
