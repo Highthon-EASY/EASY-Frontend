ac8d2f6b6eef3ef1543581ef8e6da788c9439016
강은빈 <kub0803@naver.com>
2022-01-15 22:04:09 +0900

    [FEAT] map marker

--- src/components/map/items/list/List.tsx
+++ src/components/map/items/list/List.tsx
@@ -2,90 +2,28 @@ import React from "react";
 import ListItem from "./ListItem";
 import * as S from "./style";
 
-const List = () => {
-  const listdata = [
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-    {
-      title: "원티드랩",
-      location: "송파구 올림픽로 300 롯데월드타워 35층",
-    },
-  ];
+export const listdata = [
+  {
+    title: "원티드랩",
+    location: "서울특별시 송파구 올림픽로 300 35층 롯데월드타워",
+    x: "",
+    y: "",
+  },
+  {
+    title: "짐티",
+    location: "서울특별시 강남구 역삼동 테헤란로13길 8-10 동영빌딩",
+    x: "",
+    y: "",
+  },
+  {
+    title: "opgg",
+    location: "서울특별시 강남구 언주로 637 싸이칸홀딩스타워",
+    x: "",
+    y: "",
+  },
+];
 
+const List = () => {
   return (
     <S.ListWrapper className="list-container">
       <div className="info-text">32개의 회사정보</div>
