a3e81aa17cd5404bfc43c7c6696360d74133fdc3
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-01-16 04:40:34 +0900

    Add category selector design

--- src/components/feed/feedPost/style.ts
+++ src/components/feed/feedPost/style.ts
@@ -14,13 +14,46 @@ export const PostWrapper = styled.div`
 
 export const PostBox = styled.div`
   margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  display: block;
   width: 796px;
   height: 719px;
   background-color: yellow;
+  border-radius: 5px;
+
+  & h4 {
+    margin-top: 40px;
+    margin-left: 40px;
+    margin-bottom: 15px;
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 28.96px;
+  }
+
+  & select {
+    width: 715px;
+    height: 50px;
+    border-radius: 5px;
+    border: 1px solid #5391fc;
+    box-sizing: border-box;
+    margin-left: 41px;
+    font-size: 16px;
+    line-height: 23.17px;
+    font-weight: 500;
+    padding-left: 18px;
+  }
+
+  .input-box {
+    margin-top: 36px;
+  }
 `;
-export const CategorySelect = styled.div``;
-export const TitleInput = styled.input<Props>``;
-export const ContentInput = styled.input<Props>``;
+
+export const TitleInput = styled.input<Props>`
+  width: 715px;
+  height: 50px;
+  margin-left: 41px;
+`;
+export const ContentInput = styled.input<Props>`
+  width: 715px;
+  height: 340px;
+  margin-left: 41px;
+`;
