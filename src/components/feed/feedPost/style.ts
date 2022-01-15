100326741ce5d30c2868c98abad8a208276af8f5
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-01-16 04:07:01 +0900

    Add feedpost 모달창 기본 사이즈 잡아두기

--- src/components/feed/feedPost/style.ts
+++ src/components/feed/feedPost/style.ts
@@ -4,8 +4,23 @@ interface Props {
   border: boolean;
 }
 
-export const PostWrapper = styled.div``;
-export const PostBox = styled.div``;
+export const PostWrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+export const PostBox = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 796px;
+  height: 719px;
+  background-color: yellow;
+`;
 export const CategorySelect = styled.div``;
 export const TitleInput = styled.input<Props>``;
 export const ContentInput = styled.input<Props>``;
