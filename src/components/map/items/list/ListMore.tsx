e0adda2de31a60fa373454c21259d350cda3348c
강은빈 <kub0803@naver.com>
2022-01-16 01:02:14 +0900

    [ADD] 회사 정보 컴포넌트 추가

---
+++ src/components/map/items/list/ListMore.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { modalState } from "../../../../module/atom/map";
+import * as S from "./style";
+
+const ListMore = () => {
+  const [modal, setModal] = useRecoilState(modalState);
+
+  const list = [
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+    {
+      title: "면접질문 어ㄷ쩌구",
+    },
+  ];
+
+  return (
+    <S.ModalWrapper modal={modal}>
+      <div className="border" />
+      <div className="field-box">
+        <span>면접 분야</span>
+        <span>웹 프론트엔드</span>
+      </div>
+      <div className="field-box">
+        <span>면접 난이도</span>
+        <span>면접 난이도는 중상입니다. </span>
+      </div>
+      <div className="field-box">
+        <span>면접 질문</span>
+        <ol className="interview-list">
+          {list.map((item, idx) => (
+            <li key={idx}>{item.title}</li>
+          ))}
+        </ol>
+      </div>
+    </S.ModalWrapper>
+  );
+};
+
+export default ListMore;
