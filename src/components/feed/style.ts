b027b840eb3f4eefa4927306f061bac07f6c83c0
강은빈 <kub0803@naver.com>
2022-01-16 02:11:38 +0900

    [MODIFY]

--- src/components/feed/style.ts
+++ src/components/feed/style.ts
@@ -2,39 +2,57 @@ import styled from "@emotion/styled";
 
 export const FeedWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
 `;
 
 export const FeedItem = styled.div`
-  margin: 20px 0;
+  padding: 20px;
+  box-sizing: border-box;
   display: flex;
+  flex-wrap: wrap;
   flex-direction: column;
-  width: 401px;
-  height: 186px;
+  justify-content: space-between;
+  display: flex;
+  width: 400px;
+  height: 180px;
   border: 0.5px solid;
   border-color: #cdcdcd;
   border-radius: 5px;
-  margin-left: 20px;
+  margin: 15px;
+  transition: box-shadow 0.25s ease-in 0s, transform 0.25s ease-in 0s;
 
   #title {
     font-size: 18px;
-    line-height: 26.06px;
-    letter-spacing: -2%;
     font-weight: 500;
-    padding-top: 25px;
-    padding-left: 25px;
   }
 
   .content {
-    width: 258px;
-    height: 70px;
-    display: block;
-    font-size: 15px;
-    color: #4f4f4f;
-    letter-spacing: -2%;
-    line-height: 21.72px;
-    font-weight: 400;
     margin-top: 10px;
-    margin-left: 25px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .user-info {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    span {
+      margin-right: 10px;
+    }
+    span:nth-child(1) {
+      color: #787878;
+      font-size: 14px;
+    }
+
+    span:nth-child(2) {
+      color: #b7b7b7;
+      font-size: 12px;
+    }
+  }
+
+  :hover {
+    transform: translateY(-8px);
   }
 `;
