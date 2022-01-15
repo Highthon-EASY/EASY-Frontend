a4c2210129400692c42fd322d02773a33ef8fb0c
yelim <choyr2016@naver.com>
2022-01-16 05:41:50 +0900

    [ADD] daumPost에서 location state 넘기기

--- src/components/interview/DaumPost.tsx
+++ src/components/interview/DaumPost.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import DaumPostCode from "react-daum-postcode";
 import * as S from "./style";
 import { useRecoilState } from "recoil";
 import { modalState } from "../../module/atom/interview";
+import { locationState } from "../../module/atom/interview";
 
 const DaumPost = () => {
   const [modal, setModal] = useRecoilState(modalState);
+  const [location, setLocation] = useRecoilState<string>(locationState);
+
   const handleComplete = (data: any) => {
     let fullAddress = data.address;
     let extraAddress = "";
+    let addr = "";
+    if (data.userSelectedType === "R") {
+      addr = data.roadAddress;
+      setLocation(addr);
+    } else {
+      addr = data.roadAddress;
+      setLocation(addr);
+    }
+
     if (data.addressType === "R") {
       if (data.bname !== "") {
         extraAddress += data.bname;
