c086d94987a8dc410c657cca2e7e51dae375d906
강은빈 <kub0803@naver.com>
2022-01-16 02:59:30 +0900

    [ADD] feedItem components add

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -1,32 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { feedData } from "../../module/atom/feed";
+import FeedItem from "./FeedItem";
 import * as S from "./style";
-
-//게시글 보여줌
-// 익명
-// 제목
-// 내용
-// 좋아요
 
 const FeedPage = () => {
   return (
     <S.FeedWrapper>
       {feedData.map((item: any, index: number) => (
-        <S.FeedItem key={index}>
-          <span id="title">{item.title}</span>
-          <div className="content">
-            {item.content.map((item: any) => (
-              <span>{item.title}</span>
-            ))}
-          </div>
-          <div className="user-info">
-            <span>by 익명 사용자</span>
-            <span>{item.date}</span>
-          </div>
-          <div className="like_button">
-            <img src="/assets/heart.svg" onClick={(e) => )}/>
-          </div>
-        </S.FeedItem>
+        <FeedItem key={index} item={item} />
       ))}
     </S.FeedWrapper>
   );
