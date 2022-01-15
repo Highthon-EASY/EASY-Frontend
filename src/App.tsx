710ee947a076773aedd7fc5403ac04f8d9b5cff3
yelim <choyr2016@naver.com>
2022-01-15 15:59:15 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+<<<<<<< Updated upstream
 
 import "./App.css";
 
 function App() {
   return <></>;
+=======
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+>>>>>>> Stashed changes
 }
 
 export default App;
