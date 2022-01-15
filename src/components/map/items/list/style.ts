2fb680a53db6422092acfa5e2836a40558ad363e
강은빈 <kub0803@naver.com>
2022-01-16 01:02:29 +0900

    [FEAT] modal open

--- src/components/map/items/list/style.ts
+++ src/components/map/items/list/style.ts
@@ -1,9 +1,16 @@
 import styled from "@emotion/styled";
 
+interface Props {
+  modal: boolean;
+}
+
 export const ListWrapper = styled.div`
   background: rgba(255, 255, 255, 0.75);
   overflow: auto;
   box-shadow: 7px 0px 14px rgba(164, 164, 164, 0.25);
+  z-index: 5;
+  width: 100%;
+  background: white;
 
   .info-text {
     margin-top: 60px;
@@ -41,5 +48,43 @@ export const ListItemBox = styled.div`
 
   :hover {
     background: #fafaff;
+  }
+`;
+
+export const ModalWrapper = styled.div<Props>`
+  width: 100%;
+  height: 100vh;
+  background-color: white;
+  position: absolute;
+  z-index: 3;
+  transition: transform ease 1s;
+  transform: ${({ modal }) => (modal ? `translateX(100%)` : `translateX(0)`)};
+  padding: 50px;
+  box-sizing: border-box;
+
+  .border {
+    width: 30px;
+    height: 4px;
+    background: #5391fc;
+    border-radius: 1.5px;
+    margin-top: 50px;
+  }
+
+  .field-box {
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+
+    span:nth-child(1) {
+      font-size: 18px;
+      font-weight: 500;
+    }
+
+    .interview-list {
+      li {
+        margin-left: 7%;
+        margin-top: 10px;
+      }
+    }
   }
 `;
